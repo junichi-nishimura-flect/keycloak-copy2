c6284ce06c9276789c93594ab367a4a9d2e78275
junichi-nishimura-flect <junichi.nishimura@f.flect.co.jp>
2024-12-09 15:40:57 +0900

    Revert "Backport to use clientId instead of id to save client (#30865)"
    This reverts commit 65a3a39dde3a0e1569388fdaba646631c2a412d4.

--- js/apps/admin-ui/src/components/client/ClientSelect.tsx
+++ js/apps/admin-ui/src/components/client/ClientSelect.tsx
@@ -57,7 +57,10 @@ export const ClientSelect = ({
       onFilter={(value) => setSearch(value)}
       variant={variant}
       isDisabled={isDisabled}
-      options={clients.map(({ clientId }) => clientId!)}
+      options={clients.map(({ id, clientId }) => ({
+        key: id!,
+        value: clientId!,
+      }))}
     />
   );
 };
